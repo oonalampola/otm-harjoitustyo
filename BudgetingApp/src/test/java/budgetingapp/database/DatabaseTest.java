797a4db9e65a18aea6280233d5b10c0d3feefd46
Oona <oona.lampola@helsinki.fi>
2018-05-07 17:19:23 +0300

    Testausta jatkettu

--- BudgetingApp/src/test/java/budgetingapp/database/DatabaseTest.java
+++ BudgetingApp/src/test/java/budgetingapp/database/DatabaseTest.java
@@ -7,6 +7,16 @@ package budgetingapp.database;
 
 import budgetingapp.dao.AccountDao;
 import budgetingapp.dao.UserDao;
+import budgetingapp.database.Database;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,15 +30,87 @@ import static org.junit.Assert.*;
  */
 public class DatabaseTest {
 
+    UserDao userDao;
+    AccountDao accountDao;
     Database database;
 
-    public DatabaseTest() {
-
+    public DatabaseTest() throws ClassNotFoundException {
+        this.database = new Database("jdbc:sqlite:test.db");
     }
 
-    void beforeEach() {
+    void beforeEach() throws ClassNotFoundException, SQLException {
+        File file = new File("db", "test.db");
+        file.delete();
 
+        this.database = new Database("jdbc:sqlite:test.db");
+
+        Connection c = database.getConnection();
+        initializeDatabase(c);
+
+        userDao = new UserDao(database);
+        accountDao = new AccountDao(database);
+        c.close();
     }
+
+    public void initializeDatabase(Connection c) throws SQLException {
+
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("CREATE TABLE IF NOT EXISTS User(id integer PRIMARY KEY, name varchar(200), username varchar(200));");
+        list.add("CREATE TABLE IF NOT EXISTS Account(user_id integer PRIMARY KEY, balance float)");
+        list.add("CREATE TABLE IF NOT EXISTS Event(amount float, month integer, year integer, category integer, account_id integer, FOREIGN KEY (account_id) REFERENCES Account);");
+
+        for (int i = 0; i < list.size(); i++) {
+            PreparedStatement stmt = c.prepareStatement(list.get(i));
+            stmt.execute();
+            stmt.close();
+        }
+        ArrayList<String> list2 = new ArrayList<>();
+
+        list2.add("INSERT INTO User (id, name, username) VALUES (0, 'Test user', 'testuser)");
+        list2.add("INSERT INTO Account (user_id, balance) VALUES (0, 0)");
+        list2.add("INSERT INTO Account (amount, month, year, category, account_id) VALUES (500, 1, 2018, 1, 0)");
+
+        for (int i = 0; i < list2.size(); i++) {
+            PreparedStatement stmt = c.prepareStatement(list.get(i));
+            stmt.execute();
+            stmt.close();
+        }
+    }
+//
+//    @Test
+//    public void tableUserExists() throws SQLException {
+//        
+//        Connection c = database.getConnection();
+//        PreparedStatement stmt = c.prepareStatement("SELECT * FROM User;");
+//        ResultSet rs = stmt.executeQuery();
+//        assertTrue(rs.next());
+//        c.close();
+//        rs.close();
+//
+//    }
+//    @Test
+//    public void tableAccountExists() throws SQLException {
+//        
+//        Connection c = database.getConnection();
+//        PreparedStatement stmt = c.prepareStatement("SELECT * FROM Account");
+//        ResultSet rs = stmt.executeQuery();
+//
+//        assertTrue(rs.next() != false);
+//        c.close();
+//        rs.close();
+//    }
+//    @Test
+//    public void tableEventExists() throws SQLException {
+//    
+//        Connection c = database.getConnection();
+//        PreparedStatement stmt = c.prepareStatement("SELECT * FROM Event");
+//        ResultSet rs = stmt.executeQuery();
+//
+//        assertTrue(rs.next() != false);
+//        c.close();
+//        rs.close();
+//    }
 
     @BeforeClass
     public static void setUpClass() {
@@ -39,7 +121,12 @@ public class DatabaseTest {
     }
 
     @Before
-    public void setUp() {
+    public void setUp() throws ClassNotFoundException, SQLException {
+        
+        this.database = new Database("jdbc:sqlite:test.db");
+
+        Connection c = database.getConnection();
+        initializeDatabase(c);
     }
 
     @After
